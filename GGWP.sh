3f6114844e2eacd5ae5da2d70223fac5996039d8
Larry Chiem <lchiem791@insite.4cd.edu>
2023-11-01 23:14:29 -0700

    add quotes

--- GGWP.sh
+++ GGWP.sh
@@ -7,7 +7,7 @@ ECHO "Downloading MacOS Sonoma Install Assistant"
 curl -OL --progress-bar https://swcdn.apple.com/content/downloads/39/32/042-86434-A_HKAQU4T2OA/y8aibplm2qgy1ce8ux8fxy1efgsbvprgp2/InstallAssistant.pkg
 sleep 5
 ECHO "Installing Sonoma Install Assistant"
-installer -verboseR -pkg /volumes/Macintosh HD/tmp/InstallAssistant.pkg -target "/volumes/Macintosh HD/"
+installer -verboseR -pkg "/volumes/Macintosh HD/tmp/InstallAssistant.pkg" -target "/volumes/Macintosh HD/"
 sleep 5
 cd "/volumes/Macintosh HD/applications/Install MacOS Sonoma.app/contents/macos"
 sleep 5
