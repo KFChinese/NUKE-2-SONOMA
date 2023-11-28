1ab98e8b8c64b67b75f8ef8575eced5e997ce6eb
Larry Chiem <lchiem791@insite.4cd.edu>
2023-11-28 15:17:59 -0800

    Removing Store

--- GGWP.sh
+++ GGWP.sh
@@ -57,7 +57,7 @@ kill $CAFFEINATE_PID
 
 # Install Sonoma Install Assistant
 echo "Installing Sonoma Install Assistant"
-installer -store -verboseR -dumplog -pkg "$PKG_PATH" -target /Volumes/Macintosh\ HD/
+installer -verboseR -dumplog -pkg "$PKG_PATH" -target /Volumes/Macintosh\ HD/
 if [ $? -ne 0 ]; then
     echo "Error: Failed to install InstallAssistant.pkg"
     exit 1
